f47d334d41e865bef564ff159cd9c33889b02e05
wtzhou2 <wtzhou2@iflytek.com>
2019-04-09 14:40:11 +0800

    测试111

---
+++ app/src/main/java/com/example/gittest/Utll.java
@@ -0,0 +1,14 @@
+package com.example.gittest;
+
+import android.util.Log;
+
+public class Utll {
+    private static final String TAG = "Util";
+    public static String AW;
+
+    static{
+        Log.d(TAG, "---------");
+        AW = "stringMM";
+    }
+
+}
